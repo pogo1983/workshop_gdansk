18dcb30a6646b2558c6c7bb7eac26efe61ba1658
pogo1983 <modano83@gmail.com>
2019-09-26 08:34:49 +0200

    gdfgd

---
+++ CSharp_Basics/Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Podaj pierewsza liczbe ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe ");
+            int b = int.Parse(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine("Suma to " + sum);
+            Console.ReadLine();
+            Console.ReadKey();//roznica miedzy line, ze kazdy przycisk zamknie
+
+        }
+    }
+}
