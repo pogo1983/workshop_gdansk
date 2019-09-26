912c17c79842690326505bda391908cf8081a019
pogo1983 <modano83@gmail.com>
2019-09-26 15:32:26 +0200

    next

--- CSharp_Basics/CalculatorTests/UnitTest1.cs
+++ CSharp_Basics/CalculatorTests/UnitTest1.cs
@@ -8,12 +8,48 @@ namespace CalculatorTests
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        public void AddingOperation()
+        [TestCase(1,4,5)]
+        [TestCase(5, 5, 10)]
+        public void AddingOperationPositiveValues(int a, int b, int x)
         {
-            MathData mathData = new MathData(5,4,"/");
+            //Arrange   
+            MathData mathData = new MathData(a,b,"+");
+            //Act
             var result = Calculations.DoMath(mathData);
-            Assert.AreEqual(9, result);
+            //Assert
+            Assert.AreEqual(x, result);
+        }
+
+        [Test]
+        public void AddingOperationNegativeValues()
+        {
+            //Arrange   
+            MathData mathData = new MathData(-5, -4, "+");
+            //Act
+            var result = Calculations.DoMath(mathData);
+            //Assert
+            Assert.AreEqual(-9, result);
+        }
+
+        [Test]
+        public void AddingOperationMaxInt()
+        {
+            //Arrange   
+            MathData mathData = new MathData(int.MaxValue,int.MinValue, "+");
+            //Act
+            var result = Calculations.DoMath(mathData);
+            //Assert
+            Assert.AreEqual(-1, result);
+        }
+        [Test]
+        public void AddingOperationBigInt()
+        {
+            //Arrange   
+            MathData mathData = new MathData(30000,35000, "+");
+            //Act
+            var result = Calculations.DoMath(mathData);
+            //Assert
+            Assert.AreEqual(65000, result);
         }
     }
 }
