b1fbf3d3934f23566e9206247a392ce27a28aa4e
pogo1983 <modano83@gmail.com>
2019-09-26 09:01:42 +0200

    gtrfsddsffsdadsfdsffsdads

--- CSharp_Basics/Calculator/Program.cs
+++ CSharp_Basics/Calculator/Program.cs
@@ -17,11 +17,41 @@ namespace Calculator
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine("Podaj druga liczbe ");
             int b = int.Parse(Console.ReadLine());
-            int sum = a + b;
-            Console.WriteLine("Suma to " + sum);
+            int sum = Add(a,b);
+            int sum1 = Sub(a, b);
+            int sum2 = Multilpy(a, b);
+            double sum3 = Division(a, b);
+
+            Console.WriteLine("Suma to " + sum + " Odejmowanie to " + sum1 + " Monzenie to " + sum2 + " Dzielenie to " + sum3);
             Console.ReadLine();
             Console.ReadKey();//roznica miedzy line, ze kazdy przycisk zamknie
 
         }
+
+
+        public static int Add(int a , int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            int sum = a - b;
+            return sum;
+        }
+
+        public static int Multilpy(int a, int b)
+        {
+            int sum = a * b;
+            return sum;
+        }
+
+        public static double Division(double a, double b)
+        {
+            double sum = a / b;
+            return sum;
+        }
+
     }
 }
