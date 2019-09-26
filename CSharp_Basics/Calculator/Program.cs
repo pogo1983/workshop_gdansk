72327bae16d979006383e5a8a0715dfbe472e717
pogo1983 <modano83@gmail.com>
2019-09-26 11:12:24 +0200

    dodanie funkcji dodawoani.dzielenia.mnozenia. odejmowania do metody

--- CSharp_Basics/Calculator/Program.cs
+++ CSharp_Basics/Calculator/Program.cs
@@ -17,65 +17,99 @@ namespace Calculator
             // int a = int.Parse(Console.ReadLine());
             int a = GetValueFromUser("Podaj Pierwsza liczbe");
             int b = GetValueFromUser("Podaj Druga liczbe");
-
-
+            Console.WriteLine("Podaj rodzaj dzialania czyli : + albo - albo * albo / ");
+            string operation = Console.ReadLine();
+            var sum = DoMath(a, b, operation);
+            Console.WriteLine(sum);
             //int sum = Add(a,b);
             //int sum1 = Sub(a, b);
             //int sum2 = Multilpy(a, b);
             //double sum3 = Division(a, b);
-            Console.WriteLine("Podaj rodzaj dzialania czyli : + albo - albo * albo / ");
-            string operation = Console.ReadLine();
+            //Console.WriteLine("Podaj rodzaj dzialania czyli : + albo - albo * albo / ");
+            //string operation = Console.ReadLine();
 
-            double sum=0;
-////switch
-//            switch (operation)
-//            {
-//                case "+":sum = Add(a, b);
-//                    break;
-//                case "-":sum = Sub(a, b);
-//                    break;
-//                    case "x":
-//                case "*":
-//                    sum = Multilpy(a, b);
-//                    break;
-//                case "/":
-//                    sum = Division(a, b);
-//                    break;
-//                default: Console.WriteLine("nie znaleziono");
-//                    break;
-//            }
+            //double sum=0;
+            //////switch
+            //switch (operation)
+            //{
+            //    case "+":
+            //        sum = Add(a, b);
+            //        break;
+            //    case "-":
+            //        sum = Sub(a, b);
+            //        break;
+            //    case "x":
+            //    case "*":
+            //        sum = Multilpy(a, b);
+            //        break;
+            //    case "/":
+            //        sum = Division(a, b);
+            //        break;
+            //    default:
+            //        Console.WriteLine("nie znaleziono");
+            //        break;
+            //}
 
-//else if
-            if (operation == "-")
-            {
-                sum = Sub(a, b);
-            }
-            else if (operation == "+")
-            {
-                sum = Add(a, b);
-            }
-            else if (operation == "*")
-            {
-                sum = Multilpy(a, b);
-            }
-            else if (operation == "/" )
-            {
-                sum = Division(a, b);
-            }
+            //else if
+            //if (operation == "-")
+            //{
+            //    sum = Sub(a, b);
+            //}
+            //else if (operation == "+")
+            //{
+            //    sum = Add(a, b);
+            //}
+            //else if (operation == "*")
+            //{
+            //    sum = Multilpy(a, b);
+            //}
+            //else if (operation == "/" )
+            //{
+            //    sum = Division(a, b);
+            //}
 
-            else 
-            {
-                Console.WriteLine("Error nie znalazlem dzialania");
-            }
+            //else 
+            //{
+            //    Console.WriteLine("Error nie znalazlem dzialania");
+            //}
 
-            Console.WriteLine("wynik to ");
-            Console.WriteLine(sum);
+            //Console.WriteLine("wynik to ");
+            //Console.WriteLine(sum);
 
             //Console.WriteLine("Suma to " + sum + " Odejmowanie to " + sum1 + " Monzenie to " + sum2 + " Dzielenie to " + sum3);
             // Console.WriteLine($"Suma to "{ sum } ");
             Console.ReadLine();
             Console.ReadKey();//roznica miedzy line, ze kazdy przycisk zamknie
 
+        }
+
+        public static double DoMath (int a, int b, string operation)
+        {
+            
+            
+
+            double sum = 0;
+            ////switch
+            switch (operation)
+            {
+                case "+":
+                    sum = Add(a, b);
+                    break;
+                case "-":
+                    sum = Sub(a, b);
+                    break;
+                case "x":
+                case "*":
+                    sum = Multilpy(a, b);
+                    break;
+                case "/":
+                    sum = Division(a, b);
+                    break;
+                default:
+                    Console.WriteLine("nie znaleziono");
+                    break;
+            }
+            return sum;
         }
 
         public static int GetValueFromUser(string message)
