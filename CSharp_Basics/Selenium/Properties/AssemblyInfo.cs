21792516ca92f42223bb5a27a60b8cc837d1d7c4
pogo1983 <modano83@gmail.com>
2019-09-27 08:29:46 +0200

    Start of Selenium

---
+++ CSharp_Basics/Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b89e5039-829d-4111-b6a5-48f37834325e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharp_Basics/Selenium/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace Selenium
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void FirstTest()
+            
+        {
+           var driver = new ChromeDriver();//tworzenie obiektu drivera, konstruktor
+        }
+    }
+}
