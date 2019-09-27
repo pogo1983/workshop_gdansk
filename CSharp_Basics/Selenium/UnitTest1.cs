434b8de0bc7f1cabad18e081d42e4df890540b3c
pogo1983 <modano83@gmail.com>
2019-09-27 10:07:10 +0200

    wait

--- CSharp_Basics/Selenium/UnitTest1.cs
+++ CSharp_Basics/Selenium/UnitTest1.cs
@@ -2,7 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-
+using System.Threading;
 
 namespace Selenium
 {
@@ -16,6 +16,8 @@ namespace Selenium
         public void Setup()
         {
             driver = new ChromeDriver();
+            var waitTime = new System.TimeSpan(0,0,1);
+            driver.Manage().Timeouts().ImplicitWait = waitTime;
         }
 
         [Test]
@@ -23,15 +25,26 @@ namespace Selenium
             
         {
             driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            //title="Dresses"
+            var categoryHeader = driver.FindElement(By.CssSelector("ul.sf-menu > li > a[title='Dresses']"));
+            categoryHeader.Click();
+            //Thread.Sleep(1000); czekanie ale nie ladne
+            var productCounter = driver.FindElement(By.CssSelector(".heading-counter"));
+            string text = productCounter.Text;
+
+            // text.Contains("5");mozna tak sprawdzic czy tekst zawiera jakis tekst ale lepij uzyc ponizszego StringAssert
+            StringAssert.Contains("5",text);
+
+
             //driver = new ChromeDriver;
             //var driver = new ChromeDriver();//tworzenie obiektu drivera, konstruktor
-           //Assert.AreEqual(2,1);// error zatrzymuje aplikacje
-           // driver.Quit();//close zamknie okno a quit drivera
+            //Assert.AreEqual(2,1);// error zatrzymuje aplikacje
+            // driver.Quit();//close zamknie okno a quit drivera
         }
         [TearDown]
         public void TearDown()
         {
-            driver.Quit();
+           driver.Quit();
         }
     }
 }
