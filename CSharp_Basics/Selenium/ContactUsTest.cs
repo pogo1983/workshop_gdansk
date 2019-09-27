1d89e5fb7dfc7477fc3a236f53b257c17b86594b
pogo1983 <modano83@gmail.com>
2019-09-27 11:28:55 +0200

    send contact without data

--- CSharp_Basics/Selenium/ContactUsTest.cs
+++ CSharp_Basics/Selenium/ContactUsTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,29 @@ using System.Threading.Tasks;
 namespace Selenium
 {
     [TestFixture]
-    class ContactUsTest
+    public class ContactUsTest:TestBase
     {
+        [Test]
+        public void SendFormWithoutData()
 
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            var categoryHeader = driver.FindElement(By.CssSelector("#contact-link > a:nth-child(1)"));
+            categoryHeader.Click();
+            var contactSend = driver.FindElement(By.CssSelector("#submitMessage"));
+            contactSend.Click();
+
+            var checkError = driver.FindElement(By.CssSelector(".alert"));
+            string text = checkError.Text;
+
+            StringAssert.Contains("Invalid email address.", text);
+
+
+            //driver = new ChromeDriver;
+            //var driver = new ChromeDriver();//tworzenie obiektu drivera, konstruktor
+            //Assert.AreEqual(2,1);// error zatrzymuje aplikacje
+            // driver.Quit();//close zamknie okno a quit drivera
+        }
 
     }
 }
