ce3bf41c0467eebef54801af102478d25bf0438a
pogo1983 <modano83@gmail.com>
2019-09-27 08:59:20 +0200

    selenium dalej

--- CSharp_Basics/Selenium/UnitTest1.cs
+++ CSharp_Basics/Selenium/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 
@@ -8,11 +9,29 @@ namespace Selenium
     [TestFixture]
     public class UnitTest1
     {
+        IWebDriver driver;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
         [Test]
         public void FirstTest()
             
         {
-           var driver = new ChromeDriver();//tworzenie obiektu drivera, konstruktor
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            //driver = new ChromeDriver;
+            //var driver = new ChromeDriver();//tworzenie obiektu drivera, konstruktor
+           //Assert.AreEqual(2,1);// error zatrzymuje aplikacje
+           // driver.Quit();//close zamknie okno a quit drivera
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
         }
     }
 }
